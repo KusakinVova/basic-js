92c21bce049bb629173a4bf9ba1dc3531395f8fa
Vladimir <kusakinvova@gmail.com>
2021-10-03 20:31:28 +0400

    feat: st-mine-sweeper

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,31 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper (matrix) {
+  const matrix_res = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    matrix_res.push([]);
+
+    for (let j = 0; j < matrix[i].length; j++) {
+      matrix_res[i][j] = 0;
+
+      if (matrix[i][j - 1] === true) matrix_res[i][j] += 1;
+
+      if (matrix[i][j + 1] !== undefined && matrix[i][j + 1] === true) matrix_res[i][j] += 1;
+        
+      if (matrix[i - 1] !== undefined && matrix[i - 1][j] === true) matrix_res[i][j] += 1;
+        
+      if (matrix[i + 1] !== undefined && matrix[i + 1][j] === true) matrix_res[i][j] += 1;
+
+      if (matrix[i + 1] !== undefined && matrix[i + 1][j + 1] === true) matrix_res[i][j] += 1;
+        
+      if (matrix[i + 1] !== undefined && matrix[i + 1][j - 1] === true) matrix_res[i][j] += 1;
+        
+      if (matrix[i - 1] !== undefined && matrix[i - 1][j - 1] === true) matrix_res[i][j] += 1;
+        
+      if (matrix[i - 1] !== undefined && matrix[i - 1][j + 1] === true) matrix_res[i][j] += 1;
+    }
+  }
+  return matrix_res;
 }
