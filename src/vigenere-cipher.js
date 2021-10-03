35aa26e2fff240d1a7e3aadc86b3ccdb1a44770b
Vladimir <kusakinvova@gmail.com>
2021-10-03 19:14:51 +0400

    feat: vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,59 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  alph = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  constructor(machine = true){
+    this.machine = machine;
+    return this.machine;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!(message && key)) throw new Error('Incorrect arguments!');
+    let messageSecret = [];
+    let j=0;
+    key = key.toUpperCase();
+    message = message.toUpperCase();
+
+    if(!this.machine) message = message.split('').reverse();
+
+    for(let i = 0; i < message.length; i++){
+      if(this.alph.includes(message[i])){
+        let index = ((this.alph.indexOf(message[i])+this.alph.indexOf(key[j]))%26);
+        let tmp = this.alph[index];
+        messageSecret.push(tmp);
+        j++;
+        if (j == key.length) j = 0;
+      }
+      else {
+        messageSecret.push(message[i]);
+      }
+    }
+    return messageSecret.join('');
+  }
+
+  decrypt(message, key) {
+    if (!(message && key)) throw new Error('Incorrect arguments!');
+    let messageSecret = [];
+    let j = 0;
+    key = key.toUpperCase();
+    message = message.toUpperCase();
+
+    if(!this.machine) message = message.split('').reverse();
+      
+    for(let i = 0; i < message.length; i++){
+      if(this.alph.includes(message[i])){
+        let index = (((this.alph.indexOf(message[i]) - this.alph.indexOf(key[j]))+26)%26);
+        let tmp = this.alph[index];
+        messageSecret.push(tmp);
+        j++;
+        if(j == key.length) j = 0;
+      } 
+      else {
+        messageSecret.push(message[i]);      
+      }
+    }
+    return messageSecret.join('');
   }
 }
