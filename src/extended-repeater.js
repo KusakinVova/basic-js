ab8d30b2579645242826ac79c6a807041cddadac
Vladimir <kusakinvova@gmail.com>
2021-10-03 14:11:23 +0400

    feat: extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,27 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+export default function repeater (str, options) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  if (!options["additionSeparator"]) options["additionSeparator"] = "|";
+  if (!options["separator"]) options["separator"] = "+";
+  if (!options["repeatTimes"]) options["repeatTimes"] = 1;
+  if (!options["additionRepeatTimes"]) options["additionRepeatTimes"] = 1;
+  if (options.addition === undefined) options.addition = '';
+  else options.addition = String(options.addition);
+
+  str = String(str);
+  let strResult = '';
+
+  for (let i = 1; i <= options.repeatTimes; i++) {
+    strResult += str;
+    for (let j = 1; j <= options.additionRepeatTimes; j++) {
+      strResult += options.addition;
+      if (j !== options.additionRepeatTimes) strResult += options.additionSeparator;
+    }
+    if (i !== options.repeatTimes) strResult += options.separator;
+  }
+  return strResult;
 }
